3a564f3d1476ef2dd3501c5f198e1749eb9291e7
David Berry <dberry83702@yahoo.com>
2024-02-12 22:21:37 -0600

    Initial revision of repo to demonstrate how to create Chocolatey packages

---
+++ 7zip/23.01/tools/chocolateyinstall.ps1
@@ -0,0 +1,35 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$filePath = if ((Get-OSArchitectureWidth 64) -and $env:chocolateyForceX86 -ne $true) {
+    Write-Host "Installing 64 bit version"
+    Get-Item $toolsDir\*_x64.exe 
+}
+else { 
+    Write-Error "This package only contains the 64 bit version of 7-zip.  Cannot install on 32 bit system" 
+    return
+}
+
+$packageArgs = @{
+  packageName    = '7zip'
+  fileType       = 'exe'
+  softwareName   = '7-zip*'
+  file           = $filePath
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+# This runs the installer to install the software
+Install-ChocolateyInstallPackage @packageArgs
+Remove-Item $toolsDir\*.exe -ea 0 -force
+
+# Get the installed location.  We'll need this to create the shim
+$installLocation = Get-AppInstallLocation $packageArgs.softwareName
+if (!$installLocation)  { 
+    Write-Warning "Can't find 7zip install location"; 
+    return 
+}
+Write-Host "7zip installed to '$installLocation'"
+
+# This creates the shim for the 7z executable
+Install-BinFile '7z' $installLocation\7z.exe
---
+++ bginfo/4.32/tools/chocolateyinstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$zipFilePath = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\BGInfo.zip"
+$installDir = "C:\Program Files\Sysinternals\BGInfo"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileFullPath  = $zipFilePath
+  destination   = $installDir
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+# Add it to the path
+Install-ChocolateyPath -PathToInstall $installDir -PathType 'Machine'
+
+
+# Accept the EULA
+$registryPath = 'HKCU:\SOFTWARE\Sysinternals\BGInfo'
+if (!(Test-Path $registryPath)) {
+    New-Item $registryPath -Force | Out-Null
+}
+New-ItemProperty -Path $registryPath -Name 'EulaAccepted' -Value 1 -Force | Out-Null
