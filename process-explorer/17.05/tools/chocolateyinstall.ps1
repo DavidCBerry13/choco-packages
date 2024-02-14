393490c8290315794251a59e85b623b9d48b6a54
David Berry <dberry83702@yahoo.com>
2024-02-13 21:23:37 -0600

    Add Process Explorer back in, this time with a fixed uninstall script

---
+++ process-explorer/17.05/tools/chocolateyinstall.ps1
@@ -0,0 +1,36 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$zipFilePath = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\\ProcessExplorer.zip"
+$installDir = "C:\Program Files\Sysinternals\ProcessExplorer"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileFullPath  = $zipFilePath
+  destination   = $installDir
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+
+# Not sure what this is for
+#@('procexp', 'procexp64') | ForEach-Object {
+#    New-Item "$toolsDir\$_.exe.gui" -Type File -Force | Out-Null
+#}
+
+
+# This accepts the license agreement
+$registryPath = 'HKCU:\SOFTWARE\Sysinternals\Process Explorer'
+if (!(Test-Path $registryPath)) {
+    New-Item $registryPath -Force | Out-Null
+}
+New-ItemProperty -Path $registryPath -Name 'EulaAccepted' -Value 1 -Force | Out-Null
+
+
+# This will make a shortcut in C:\ProgramData\Microsoft\Windows\Start Menu\Programs
+$shortcutPath = Join-Path $([Environment]::GetFolderPath([System.Environment+SpecialFolder]::CommonPrograms)) 'Process Explorer.lnk'
+if (-not (Test-Path $shortcutPath)) {
+    $targetPath = if ([Environment]::Is64BitOperatingSystem) { 'procexp64.exe' } else { 'procexp.exe' }
+    Install-ChocolateyShortcut -ShortcutFilePath $shortcutPath -TargetPath "$installDir\$targetPath"
+}
---
+++ process-explorer/17.05/tools/chocolateyuninstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$zipArchive = 'ProcessExplorer.zip'
+$installDir = "C:\Program Files\Sysinternals\ProcessExplorer"
+$registryPath = 'HKCU:\SOFTWARE\Sysinternals\Process Explorer'
+
+
+# This removes the files from the directory
+Uninstall-ChocolateyZipPackage -Packagename $env:ChocolateyPackageName -ZipFileName $zipArchive
+
+# Uninstall-ChocolateyZipPackage only removes the files unzipped from the archive
+# To Remove the directory the files were unzipped to, you need to do this separetly in the script
+Remove-Item $installDir
+
+# Clean up any registry info
+if (Test-Path $registryPath) {
+    Remove-Item $sysinternalsPath -Recurse -Force
+}
+
+# Clean up the shortcut
+$shortcutPath = Join-Path $([Environment]::GetFolderPath([System.Environment+SpecialFolder]::CommonPrograms)) 'Process Explorer.lnk'
+if (Test-Path $shortcutPath) {
+    Remove-Item $shortcutPath
+}
